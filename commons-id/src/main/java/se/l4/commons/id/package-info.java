2f8bc4b1b038591b1854f905b4ae1b023e0e82ef
Andreas Holstenson <a@holstenson.se>
2018-09-07 10:33:42 +0200

    docs(id): Adding short package documentation

---
+++ commons-id/src/main/java/se/l4/commons/id/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Generation, coding and decoding of identifiers. Currently works with
+ * {@code long}-identifiers, via {@link LongIdGenerator} and {@link LongIdCodec}.
+ */
+package se.l4.commons.id;
