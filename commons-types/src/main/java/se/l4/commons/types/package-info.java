31ac5e45678bee335f4d50396a0355988571775f
Andreas Holstenson <a@holstenson.se>
2018-09-07 10:33:49 +0200

    docs(types): Adding short package documentation

---
+++ commons-types/src/main/java/se/l4/commons/types/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Type finding, creation and manipulation. This package provides tools to work
+ * with {@link TypeFinder find types}, {@link InstanceFactory create instances},
+ * to {@link se.l4.commons.types.proxies.Proxies create proxy types} and to
+ * {@link Types work with generics}.
+ */
+package se.l4.commons.types;
