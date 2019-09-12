080100e40509e6bc3309f419267d3f8c561c4237
Andreas Holstenson <a@holstenson.se>
2019-09-12 08:36:21 +0200

    fix(types): equals and hashCode for DefaultMatchedType

--- commons-types/src/main/java/se/l4/commons/types/matching/DefaultMatchedType.java
+++ commons-types/src/main/java/se/l4/commons/types/matching/DefaultMatchedType.java
@@ -32,4 +32,36 @@ public class DefaultMatchedType<T, D>
 	{
 		return "DefaultMatchedType{" + type + " => " + data + "}";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DefaultMatchedType other = (DefaultMatchedType) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+
 }
